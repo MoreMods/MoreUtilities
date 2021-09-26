154f836adf3182ce9d3734052f51bd4b696299c8
saevich <46279702+saevich@users.noreply.github.com>
2021-09-26 10:46:37 +0300

    build 0-0-alpha-3
    Added command /cleardrop.
    This command clear all item entities (items, that are on ground) and keybind (numpad add, but every user can edit it in Control settings in the game)

---
+++ src/main/java/ru/ardev/moreutilities/procedures/ClearDropProcedureProcedure.java
@@ -0,0 +1,65 @@
+package ru.ardev.moreutilities.procedures;
+
+import ru.ardev.moreutilities.world.ClearDropLightningEffectGameRule;
+import ru.ardev.moreutilities.MoreutilitiesModElements;
+import ru.ardev.moreutilities.MoreutilitiesMod;
+
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.World;
+import net.minecraft.world.IWorld;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.util.math.vector.Vector2f;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.effect.LightningBoltEntity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.Entity;
+import net.minecraft.command.ICommandSource;
+import net.minecraft.command.CommandSource;
+
+import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
+
+@MoreutilitiesModElements.ModElement.Tag
+public class ClearDropProcedureProcedure extends MoreutilitiesModElements.ModElement {
+	public ClearDropProcedureProcedure(MoreutilitiesModElements instance) {
+		super(instance, 6);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				MoreutilitiesMod.LOGGER.warn("Failed to load dependency entity for procedure ClearDropProcedure!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				MoreutilitiesMod.LOGGER.warn("Failed to load dependency world for procedure ClearDropProcedure!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		IWorld world = (IWorld) dependencies.get("world");
+		if (world instanceof ServerWorld) {
+			((World) world).getServer().getCommandManager().handleCommand(
+					new CommandSource(ICommandSource.DUMMY, new Vector3d((entity.getPosX()), (entity.getPosY()), (entity.getPosZ())), Vector2f.ZERO,
+							(ServerWorld) world, 4, "", new StringTextComponent(""), ((World) world).getServer(), null).withFeedbackDisabled(),
+					"kill @e[type=item]");
+		}
+		{
+			List<? extends PlayerEntity> _players = new ArrayList<>(world.getPlayers());
+			for (Entity entityiterator : _players) {
+				if (((world.getWorldInfo().getGameRulesInstance().getBoolean(ClearDropLightningEffectGameRule.gamerule)) == (true))) {
+					if (world instanceof ServerWorld) {
+						LightningBoltEntity _ent = EntityType.LIGHTNING_BOLT.create((World) world);
+						_ent.moveForced(Vector3d.copyCenteredHorizontally(
+								new BlockPos((int) (entityiterator.getPosX()), (int) (entityiterator.getPosY()), (int) (entityiterator.getPosZ()))));
+						_ent.setEffectOnly(true);
+						((World) world).addEntity(_ent);
+					}
+				}
+			}
+		}
+	}
+}
